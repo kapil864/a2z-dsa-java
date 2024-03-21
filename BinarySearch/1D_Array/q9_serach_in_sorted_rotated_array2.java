cec681d70446930895290fa3a231d185fe43427b
kapilTantuway <kapil.kumar.svp@gmail.com>
2024-03-21 10:12:10 +0530

    search min and element in sorted rotated array

--- BinarySearch/1D_Array/q9_serach_in_sorted_rotated_array2.java
+++ BinarySearch/1D_Array/q9_serach_in_sorted_rotated_array2.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class q8_serach_in_sorted_rotated_array {
+public class q9_serach_in_sorted_rotated_array2 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int size = sc.nextInt();
@@ -15,15 +15,30 @@ public class q8_serach_in_sorted_rotated_array {
         sc.close();
     }
 
-    // Optimal Approach
-    // TC = LogN
+    // TC = O(LogN) for best and average case
     private static int searchOptimal(int[] arr, int key) {
         int st = 0;
         int ed = arr.length - 1;
         while (st <= ed) {
             int mid = (st + ed) / 2;
+
+            // // TC = O(N) for worst case
+            // // when using two while loops for discarding equal values
+            // while(arr[st] == arr[mid] && st < mid){
+            // st++;
+            // }
+            // while (arr[ed]== arr[mid] && ed > mid) {
+            // ed--;
+            // }
             if (arr[mid] == key) {
                 return mid;
+            }
+
+            // using this if condition two values will be discarded at once
+            // Worst case time complexity with this case is O(N/2)
+            else if (arr[mid] == arr[st] && arr[mid] == arr[ed]) {
+                st++;
+                ed--;
             } else if (arr[st] <= arr[mid]) {
                 if (key >= arr[st] && key <= arr[mid]) {
                     ed = mid - 1;
@@ -31,14 +46,14 @@ public class q8_serach_in_sorted_rotated_array {
                     st = mid + 1;
                 }
             } else {
-                if(key <= arr[ed] && key >= arr[mid]){
-                    st = mid +1;
-                }
-                else{
-                    ed = mid -1;
+                if (key <= arr[ed] && key >= arr[mid]) {
+                    st = mid + 1;
+                } else {
+                    ed = mid - 1;
                 }
             }
         }
         return -1;
     }
+
 }
